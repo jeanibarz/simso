df0c59f3c8cc860e8a0dd8448a8233404187afee
Jean Ibarz <jean.ibarz@continental-corporation.com>
2018-11-06 18:31:54 +0100

    Added test to generate aperiodic tasks set

---
+++ tests/func_tests/generator_tests/task_generator_tests/test_gen_kato_aperiodic_tasks_set.py
@@ -0,0 +1,13 @@
+from simso.generator.task_generator import gen_kato_aperiodic_tasks_set
+
+
+def test_generating_set():
+    task_info_list = gen_kato_aperiodic_tasks_set(total_utilization=1.0,
+                                                  total_duration=1000,
+                                                  periods_dist={'dist_type': 'uniform', 'min': 100, 'max': 1000, 'round_to_integer': True},
+                                                  max_utilization_relative_error=0.05,
+                                                  values_dist={'dist_type': 'uniform', 'min': 100, 'max': 1000, 'round_to_integer': False},
+                                                  firm_tol_dist={'dist_type': 'uniform', 'min': 100, 'max': 1000, 'round_to_integer': True},
+                                                  soft_tol_dist={'dist_type': 'uniform', 'min': 100, 'max': 1000, 'round_to_integer': True})
+    assert len(task_info_list) > 0
+
